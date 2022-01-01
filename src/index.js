f2207d91b0f229cdf8455f7292309eeb879436b2
Haroon Feisal <haroonf@haroon-mbp.local>
2021-12-31 19:09:07 -0500

    Added fontsource to reduce loading times.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { ColorModeScript } from '@chakra-ui/react';
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import '@fontsource/roboto-mono';
 
 ReactDOM.render(
   <StrictMode>
