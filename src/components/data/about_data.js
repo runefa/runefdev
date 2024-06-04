52a6fec66b1c0570bcd0624af42ae495f850ed96
Rune F <rune@runes-laptop.lan>
2024-06-03 22:16:39 -0700

    Updated website with latest resume and experiences.

--- src/components/data/about_data.js
+++ src/components/data/about_data.js
@@ -5,4 +5,4 @@ I found the challenge of programming to be exciting. In
 middle school, I was invited to attend a Python bootcamp
 where my passion for software was cemented.`;
 
-export const sideText = `Outside of coding, I enjoy weightlifting, biking, gaming, and reading. `;
+export const sideText = `Outside of coding I enjoy weightlifting, hiking, photography, and reading. `;
--- src/components/data/experience_data.js
+++ src/components/data/experience_data.js
@@ -43,11 +43,36 @@ export const experience_data = {
         ],
       ],
     },
-    'Software Engineer I': {
+    'UI/UX Software Engineer': {
       location: 'Redmond WA',
-      dates: 'Feb 2022 - Current',
-      description: [],
-      badges: [],
+      dates: 'Feb 2022 - Nov 2022',
+      description: [
+        'Drove the design and release of the Azure Container Apps CLI command module from private beta versions to public announcement including an end-to-end fully managed deployment experience.',
+        'Developed and maintained all Azure App Service CLI commands, catering to a combined 2 million monthly active users.',
+      ],
+      badges: [
+        ['https://github.com/Azure/azure-cli','Azure CLI'],
+        ['https://www.python.org/about/','Python'],
+        ['https://github.com/microsoft/knack','Knack'],
+        ['https://azure.microsoft.com/en-us/products/container-apps', 'Container Apps'],
+      ],
     },
+    'Backend Software Engineer': {
+      location: 'Redmond WA',
+      dates: 'Nov 2022 - Current',
+      description: [
+        'Architected and deployed robust automated infrastructure across over sixty regions for Azure App Service Managed Certificates that reduces sync time from hours to minutes.',
+        'Led the design and implementation of the regionalization of Azure App Service Domains involving the complete overhaul of the database and data layers.',
+        'Oversaw maintenance and support operations for Azure App Service Domains serving over 80,000 yearly customers while spearheading efforts to enhance fraud detection through increased alerting and the implementation of a new customer trust system.',
+      ],
+      badges: [
+        ['https://learn.microsoft.com/en-us/azure/app-service/manage-custom-dns-buy-domain','App Service Domains'],
+        ['https://dotnet.microsoft.com/en-us/learn/dotnet/what-is-dotnet','.NET'],
+        ['https://azure.microsoft.com/en-us/products/cosmos-db','CosmosDB (NoSQL)'],
+        ['https://azure.microsoft.com/en-us/products/azure-sql/database','Azure SQL'],
+        ['https://learn.microsoft.com/en-us/azure/azure-resource-manager/management/overview','ARM'],
+      ],
+    },
+
   },
 };
